011339584048db424e623c063a96342f1fc37bb8
Yanelis Serrano <yaneliserranotaylor@gmail.com>
2023-03-29 09:29:27 +0200

    Add task master functions

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,28 @@
-import Header from './Header';
-import CalendarFrame from './CalendarFrame';
-import '../styles/App.scss';
+import { useState } from 'react';
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import events from "./event";
+
+const localizer = momentLocalizer(moment);
 
 
 function App() {
 
+  const [eventList, setEventList] = useState(events)
 
-
-
-  
- 
-
-
-
-  return <div className="App">{
-    <>
-    <Header/>
-    <main>
-    <CalendarFrame/>
-    </main>
-    </>
-
-
-
-
-  }</div>;
+  return (
+    <div className="App">
+      <Calendar
+        localizer={localizer}
+        defaultDate={new Date()}
+        defaultView="month"
+        events={eventList}
+        style={{ height: "100vh" }}
+      />
+    </div>
+  );
 }
 
-/* PROP-TYPES */
 
-/* EXPORT DEL COMPONENTE */
 export default App;
