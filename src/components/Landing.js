5d28d87d540fa06f586888f5e7ee36e00966e303
Nuria Castaño <codesalnuria@gmail.com>
2023-08-01 11:39:33 +0200

    add apiLogin function in services folder

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,25 +1,38 @@
 import { Link } from "react-router-dom";
 import "../styles/Landing.scss";
+import { useState } from "react";
 
 function Landing() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+  }
+
+
   return (
     <main className="mainLanding">
       <section className="mainLanding__content">
         <h3 className="mainLanding__content__title">Iniciar sesión</h3>
-        <label className="mainLanding__content__user">
-          <i className="fa-solid fa-user"></i>
-          Usuario
-        </label>
-        <label className="mainLanding__content__password">
-          <i className="fa-solid fa-lock"></i>Contraseña
-        </label>
-        <label className="mainLanding__content__login">
-          <Link className="mainLanding__content__login--text" to="/createtask">
-            Entrar
-          </Link>
-        </label>
+        <form onSubmit={handleSubmit}>
+          <label for="email" className="mainLanding__content__user">
+            <i className="fa-solid fa-user"/>
+            <input type="email" id="email" name="name" required/>
+            Correo electrónico
+          </label>
+          <label for="password" className="mainLanding__content__password">
+            <i className="fa-solid fa-lock"/>
+            <input type="password" id="password" name="password" required/>
+            Contraseña
+          </label>
+          <label className="mainLanding__content__login">
+            <input className="mainLanding__content__login--text" type="submit" value="Entrar"/>
+          </label>
+        </form>
         <p className="mainLanding__content__signup">
-          Nuevo por aquí? Regístrate
+          ¿Nuevo por aquí? Regístrate
         </p>
       </section>
     </main>
