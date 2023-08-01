cb875a83a67c6b13c3803d3ab112b67a0e1692a3
Nuria Castaño <codesalnuria@gmail.com>
2023-08-01 12:46:52 +0200

    new form

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,8 +4,52 @@ import { useState } from "react";
 
 function Landing() {
 
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChangeInput = (inputValue, inputName) => {
+    setEmail({
+      ...email, [inputName]: inputValue,
+    });
+    setPassword({
+      ...password, [inputName]: inputValue,
+    });
+  }
+
+  const handleClick = async () => {
+    try {
+      if (!email || !password) {
+        alert('Por favor, ingresa el email y la constraseña');
+        return;
+      }
+      const data = {
+        email: email,
+        password: password,
+      };
+      const response = await fetch('http://localhost:3000/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+
+      if(!response.ok){
+        alert('Inicio de sesión fallido. Por favor, verifica tu email y contraseña');
+        return;
+      }
+      const responseData = await response.json();
+      if(responseData && responseData.token) {
+        alert('Inicio de sesión con éxito. Token obtenido');
+      } else {
+        alert ('Inicio de sesión fallido. or favor, verifica tus credenciales')
+      }
+    } catch(error){
+      console.error('Error al procesar la solicitud:', error);
+      alert('Ocurrió un error al procesar la solicitud. Por favor, intentalo de nuevo.')
+    }
+  }
+
 
   const handleSubmit = (ev) => {
     ev.preventDefault();
@@ -17,18 +61,35 @@ function Landing() {
       <section className="mainLanding__content">
         <h3 className="mainLanding__content__title">Iniciar sesión</h3>
         <form onSubmit={handleSubmit}>
-          <label for="email" className="mainLanding__content__user">
+          <label htmlFor="email" className="mainLanding__content__user">
             <i className="fa-solid fa-user"/>
-            <input type="email" id="email" name="name" required/>
+            <input 
+            htmlFor={"email"}
+            type="email" 
+            id="email" 
+            name="name" 
+            placeholder={"ejemplo@gmail.com"}
+            value={email}
+            handleChangeInput={handleChangeInput}
+            required/>
             Correo electrónico
           </label>
           <label for="password" className="mainLanding__content__password">
             <i className="fa-solid fa-lock"/>
-            <input type="password" id="password" name="password" required/>
+            <input type="password" id="password" 
+            name="password"
+            placeholder={"contraseña"}
+            value={password}
+            handleChangeInput={handleChangeInput}
+            required/>
             Contraseña
           </label>
           <label className="mainLanding__content__login">
-            <input className="mainLanding__content__login--text" type="submit" value="Entrar"/>
+            <input className="mainLanding__content__login--text" 
+            type="submit" 
+            value="Entrar"
+            onClick={handleClick}
+            />
           </label>
         </form>
         <p className="mainLanding__content__signup">
